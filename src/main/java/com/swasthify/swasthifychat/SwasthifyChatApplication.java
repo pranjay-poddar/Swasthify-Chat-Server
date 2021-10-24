aebf8b810113f89957d01e67cc4b525b468439ed
pranjay-poddar <pranjaypoddar@outlook.com>
2021-10-24 13:11:41 +0530

    generated dependencies

---
+++ src/main/java/com/swasthify/swasthifychat/SwasthifyChatApplication.java
@@ -0,0 +1,13 @@
+package com.swasthify.swasthifychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwasthifyChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwasthifyChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swasthify/swasthifychat/SwasthifyChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swasthify.swasthifychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwasthifyChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
